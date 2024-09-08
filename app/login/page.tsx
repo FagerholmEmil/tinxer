5b6260862523f613f353313799a2ecdb421dfe19
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-09-08 11:56:59 -0700

    github auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,48 @@
-import { Input } from "@/components/ui/input";
-import { createClient } from "@/utils/supabase/server";
+"use client";
+import { Button } from "@/components/ui/button";
+import { createClientBrowser } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const useUser = () => {
+  const supabase = createClientBrowser();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN" && session) {
+        setUser(session.user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, [supabase.auth]);
+
+  return user;
+};
 
 export default function Login() {
-  const supabase = createClient();
-  supabase.auth.signUp({ email: "test@test.com", password: "12345678" });
+  const supabase = createClientBrowser();
+
+  const handleSubmit = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: `${window.location.origin}/auth/callback`,
+      },
+    });
+
+    if (data.url) {
+      redirect(data.url); // use the redirect API for your server framework
+    }
+  };
+
+  const user = useUser();
 
   return (
-    <form>
-      <Input type="email" placeholder="test@test.com" />
-      <Input type="password" placeholder="12345678" />
-    </form>
+    <div>
+      <Button onClick={handleSubmit}>Login with Github</Button>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,9 @@
-"use client";
-
-import React, { useState, useCallback, useEffect } from "react";
-import ArxivFetcher from "./components/ArxivFetcher";
-import CustomPDFViewer from "./components/CustomPDFViewer";
-import { getLikedPdfs } from "./utils/pdfUtils";
 import Login from "./login/page";
 
 const PDFBrowser: React.FC = () => {
-  const [pdfLinks, setPdfLinks] = useState<string[]>([]);
-  const [likedPdfs, setLikedPdfs] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchLikedPdfs = async () => {
-      const oldLikedPdfs = await getLikedPdfs();
-      setLikedPdfs(oldLikedPdfs);
-    };
-    fetchLikedPdfs();
-  }, []);
-
-  const handlePdfLinksUpdate = (links: string[]) => {
-    setPdfLinks(links);
-  };
-
-  const handleLikedPdfUpdate = useCallback((newLikedPdf: string) => {
-    setLikedPdfs((prev) => [...prev, newLikedPdf]);
-  }, []);
-
   return (
     <div className="flex flex-row justify-center items-center min-h-[50vh] w-full">
       <Login />
-      <div className="w-1/2 m-4 rounded-lg bg-white shadow-xl">
-        <ArxivFetcher onPdfLinksUpdate={handlePdfLinksUpdate} />
-        <CustomPDFViewer
-          pdfUrls={pdfLinks}
-          onLikedPdfUpdate={handleLikedPdfUpdate}
-        />
-      </div>
     </div>
   );
 };
