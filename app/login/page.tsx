8a5ce64de65bfbd24c3d9bb532358732fca1e2f5
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-09-08 11:23:24 -0700

    login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,14 @@
+import { Input } from "@/components/ui/input";
 import { createClient } from "@/utils/supabase/server";
 
 export default function Login() {
   const supabase = createClient();
   supabase.auth.signUp({ email: "test@test.com", password: "12345678" });
 
-  return;
+  return (
+    <form>
+      <Input type="email" placeholder="test@test.com" />
+      <Input type="password" placeholder="12345678" />
+    </form>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-import React, { useState, useCallback, useEffect } from 'react';
-import ArxivFetcher from './components/ArxivFetcher';
-import CustomPDFViewer from './components/CustomPDFViewer';
-import { getLikedPdfs } from './utils/pdfUtils';
+import React, { useState, useCallback, useEffect } from "react";
+import ArxivFetcher from "./components/ArxivFetcher";
+import CustomPDFViewer from "./components/CustomPDFViewer";
+import { getLikedPdfs } from "./utils/pdfUtils";
+import Login from "./login/page";
 
 const PDFBrowser: React.FC = () => {
   const [pdfLinks, setPdfLinks] = useState<string[]>([]);
@@ -22,14 +23,18 @@ const PDFBrowser: React.FC = () => {
   };
 
   const handleLikedPdfUpdate = useCallback((newLikedPdf: string) => {
-    setLikedPdfs(prev => [...prev, newLikedPdf]);
+    setLikedPdfs((prev) => [...prev, newLikedPdf]);
   }, []);
 
   return (
-    <div className='flex flex-row justify-center items-center min-h-[50vh] w-full'>
-      <div className='w-1/2 m-4 rounded-lg bg-white shadow-xl'>
+    <div className="flex flex-row justify-center items-center min-h-[50vh] w-full">
+      <Login />
+      <div className="w-1/2 m-4 rounded-lg bg-white shadow-xl">
         <ArxivFetcher onPdfLinksUpdate={handlePdfLinksUpdate} />
-        <CustomPDFViewer pdfUrls={pdfLinks} onLikedPdfUpdate={handleLikedPdfUpdate} />
+        <CustomPDFViewer
+          pdfUrls={pdfLinks}
+          onLikedPdfUpdate={handleLikedPdfUpdate}
+        />
       </div>
     </div>
   );
