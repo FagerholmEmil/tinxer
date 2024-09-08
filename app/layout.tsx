e3ef25d8fb535cd34525e1a6bbb63ae4fce1e999
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2024-09-08 12:58:47 -0700

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
